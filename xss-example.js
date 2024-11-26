4b39a481ff6a0b3e4548a21e83c7afd6542f9e6b
lior-mobb <lior@mobb.ai>
2024-11-26 14:26:08 +0100

    Update xss-example.js

--- xss-example.js
+++ xss-example.js
@@ -3,3 +3,7 @@ var urlParams = new URLSearchParams(window.location.search);
 var username = urlParams.get('username');
 
 document.write("Hell11111o to you!!!!!!!, " + username + "!!!");
+
+var name = urlParams.get("name");
+var unsafe_div = window.document.getElementById("vulnerable-div");
+unsafe_div.innerHTML = "Hello " + name;
